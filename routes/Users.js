b41c3589fee7d9d4eb5d4d4f8175561abe3331bb
ciamac <hgzguzg>
2020-06-21 19:45:06 +0200

    Verified by Cia v1.0.5 register backend!

--- routes/Users.js
+++ routes/Users.js
@@ -14,7 +14,24 @@ users.post("/register",(req,res) => {
       const today = new Date(),
       const userData ={
             first_name : req.body.first_name,
-            last_name: req.body.last_name
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password,
+            created: today,
       }
+User.findOne({
+      email: req.body.email
+})
+.then(user =>{
+      if(!user){
+            bcrypt.hash(req.body.password ,10 , (err,hash)=>{
+                  userData.password = hash  
+                  User.create(userData)
+                  .then(user=>{
+                        res.json({status:user.email + "registered!"})
+                  })
+            })
+      }
+})
 
 })
