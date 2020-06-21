ce0f634f45a95ae29884f716bd5ebfd9e4b982fc
ciamac <hgzguzg>
2020-06-21 17:01:14 +0200

    Verified by Cia v1.0.2

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -5,3 +6,20 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000
 
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+      bodyParser.urlencoded({
+            extended: false
+      })
+)
+
+mongoose
+.connect(
+      process.env.MongoURL, { 
+            useNewUrlParser:    true,
+            useCreateIndex:     true,
+            useUnifiedTopology: true
+          })
+.then(() => console.log("MongoDB connected!"))
+.catch(err => console.log(err))
