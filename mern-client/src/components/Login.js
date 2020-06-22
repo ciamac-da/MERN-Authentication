b605833e311489b2cae96af538e1d70e38cc063c
ciamac <hgzguzg>
2020-06-22 13:43:02 +0200

    Verified by Cia v1.1.5 Login finished?

--- mern-client/src/components/Login.js
+++ mern-client/src/components/Login.js
@@ -29,4 +29,48 @@ onSubmit(e){
       })
 }
 
+render() {
+      return (
+            <div className="container">
+                  <div className="row">
+                        <div className="col-md-6 mt-5 mx-auto">
+                              <form noValidate onSubmit={this.onSubmit}>
+                              <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                              <div className="form-group">
+                              <label htmlFor="email">Email Address</label>
+                              <input 
+                              type="email"
+                              className="form-control"
+                              name="email"
+                              placeholder="Enter Email"
+                              value={this.state.email}
+                              onChange={this.onChange}
+                              />
+                              </div>
+
+
+                              <div className="form-group">
+                              <label htmlFor="password">Password</label>
+                              <input 
+                              type="password"
+                              className="form-control"
+                              name="password"
+                              placeholder="Enter Password"
+                              value={this.state.password}
+                              onChange={this.onChange}
+                              />
+                              </div>
+                              <button 
+                              className="btn btn-lg btn-primary btn-block"
+                              type="submit"
+                              >
+                                    Sign In
+                              </button>
+                              </form>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
 }
